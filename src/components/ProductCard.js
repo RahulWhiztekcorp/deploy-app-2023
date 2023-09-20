0515dbb375f2d9f6086ce674dbcb14bccbb3d756
RahulWhiztekcorp <rahul.whiztekcorp@gmail.com>
2023-09-20 18:56:48 +0530

    Rupee symbol changed in productcard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -7,7 +7,7 @@ const ProductCard = ({ title, price, image, rating }) => {
         <div className='card ' style={{ padding: 20, width: '270px', borderRadius: '5px'}}>
             <img style={{ width: '100%', height: '180px', objectFit: 'contain' ,borderRadius: '5px'}} src={image} />
             <p >{title}</p>
-            <h4>${price}</h4>
+            <h4>₹{price}</h4>
             <h6><span class="badge text-bg-success">{rating.rate}<i class="fa-sharp fa-solid fa-star"></i></span></h6>
             <div className='d-flex justify-content-between mt-2'>
                 <a href="#" class="btn btn-warning">Add To Cart</a>
